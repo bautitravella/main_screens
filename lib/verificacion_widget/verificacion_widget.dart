fb5315c43d3b8476ed25b15c2983d17bfe21b1e6
bautitravella <jbttravellita@gmail.com>
2020-01-06 02:39:29 -0300

    solo subo el verificacion y el gitignore

--- lib/verificacion_widget/verificacion_widget.dart
+++ lib/verificacion_widget/verificacion_widget.dart
@@ -4,18 +4,18 @@ import 'package:flutterui/values/values.dart';
 
 
 class VerificacionWidget extends StatelessWidget {
-  
+
   void onBtnBlueTwoPressed(BuildContext context) {
-  
+
   }
-  
+
   void onBtnBluePressed(BuildContext context) {
-  
+
   }
-  
+
   @override
   Widget build(BuildContext context) {
-  
+
     return Scaffold(
       body: Container(
         constraints: BoxConstraints.expand(),
@@ -25,77 +25,69 @@ class VerificacionWidget extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: 24,
-              decoration: BoxDecoration(
-                color: AppColors.secondaryElement,
-              ),
-              child: Container(),
-            ),
-            Container(
-              height: 46,
-              margin: EdgeInsets.only(left: 28, top: 61, right: 80),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Verificacion",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: AppColors.accentText,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 38,
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 230,
+                height: 46,
+                margin: EdgeInsets.only(left: 28, top: 85),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Verificacion",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: AppColors.accentText,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 38,
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Align(
+                    Align(
                       alignment: Alignment.topLeft,
                       child: Container(
+                        width: 29,
                         height: 29,
-                        margin: EdgeInsets.only(left: 32, top: 13),
+                        margin: EdgeInsets.only(left: 10, top: 12),
                         child: Image.asset(
                           "assets/images/icons8-synchronize-100px.png",
                           fit: BoxFit.none,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Expanded(
               flex: 1,
               child: Container(
-                margin: EdgeInsets.only(left: 59, top: 78, right: 58, bottom: 38),
+                margin: EdgeInsets.only(left: 59, top: 78, right: 58),
                 child: Image.asset(
                   "assets/images/gmail-amarillopng.png",
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                margin: EdgeInsets.only(top: 38),
-                child: Text(
-                  "Revisa tu correo",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: AppColors.primaryText,
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.w700,
-                    fontSize: 26,
-                    letterSpacing: -0.41786,
-                  ),
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 96, top: 12, right: 95),
+              margin: EdgeInsets.only(left: 73, right: 73, bottom: 17),
+              child: Text(
+                "Revisa tu correo",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.primaryText,
+                  fontFamily: "Montserrat",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 26,
+                  letterSpacing: -0.41786,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 73, right: 77, bottom: 82),
               child: Text(
                 "Se a enviado un mail a tu correo \ny abre el link para que podamos\ncomprobar la dirección ingresada.",
                 textAlign: TextAlign.center,
