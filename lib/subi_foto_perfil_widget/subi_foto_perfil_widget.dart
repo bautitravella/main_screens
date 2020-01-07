b3b174c75c14576b79b951702cc9726597c94076
bautitravella <jbttravellita@gmail.com>
2020-01-07 02:13:28 -0300

    image screen update

--- lib/subi_foto_perfil_widget/subi_foto_perfil_widget.dart
+++ lib/subi_foto_perfil_widget/subi_foto_perfil_widget.dart
@@ -37,11 +37,12 @@ class SubiFotoPerfilWidget extends StatelessWidget {
               margin: EdgeInsets.only(top: 23),
               child: Image.asset(
                 "assets/images/phonochico.png",
-                fit: BoxFit.cover,
+                fit: BoxFit.fill,
               ),
             ),
+            Spacer(),
             Container(
-              margin: EdgeInsets.only(left: 92, top: 5, right: 91),
+              margin: EdgeInsets.only(left: 92, right: 91, bottom: 12),
               child: Text(
                 "Subí una foto",
                 textAlign: TextAlign.center,
@@ -54,21 +55,18 @@ class SubiFotoPerfilWidget extends StatelessWidget {
                 ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: EdgeInsets.only(left: 108, top: 12, right: 107, bottom: 164),
-                child: Text(
-                  "Es importante subir una foto \nen la que salgas bien.\n",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 118, 118, 118),
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.w400,
-                    fontSize: 10,
-                    letterSpacing: -0.1,
-                    height: 1.4,
-                  ),
+            Container(
+              margin: EdgeInsets.only(left: 108, right: 107, bottom: 164),
+              child: Text(
+                "Es importante subir una foto \nen la que salgas bien.\n",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 118, 118, 118),
+                  fontFamily: "Montserrat",
+                  fontWeight: FontWeight.w400,
+                  fontSize: 10,
+                  letterSpacing: -0.1,
+                  height: 1.4,
                 ),
               ),
             ),
