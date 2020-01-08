fe09d6321474a5605b9ddd22efa5494c97132e3d
Bauti <jbttravellita@gmail.com>
2020-01-08 16:27:09 -0300

    La Cagamos

--- lib/firstscreen_widget/firstscreen_widget.dart
+++ lib/firstscreen_widget/firstscreen_widget.dart
@@ -107,7 +107,7 @@ class FirstscreenWidget extends StatelessWidget {
                   textColor: Color.fromARGB(255, 255, 255, 255),
                   padding: EdgeInsets.all(25),
                   child: Text(
-                    "SE QUEDA4",
+                    "La cagamos1",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Color.fromARGB(255, 255, 255, 255),
