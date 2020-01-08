ceb80e21a295e5bac708085645cfdb516713eba7
agustormakh <agustormakh@gmail.com>
2020-01-08 14:55:40 -0300

    first screen widget

--- lib/firstscreen_widget/firstscreen_widget.dart
+++ lib/firstscreen_widget/firstscreen_widget.dart
@@ -73,7 +73,7 @@ class FirstscreenWidget extends StatelessWidget {
                       child: Align(
                         alignment: Alignment(0.0, -0.5),
                         child: Text(
-                          "LOG IN",
+                          "Agustin",
                           style: TextStyle(
                             color: Color.fromARGB(255, 255, 255, 255),
                             fontFamily: "Montserrat",
