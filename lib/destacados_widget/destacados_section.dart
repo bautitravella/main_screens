7954084e8cf927f6719f0fcc74cba20480d58b1a
bautitravella <jbttravellita@gmail.com>
2020-01-12 19:20:31 -0300

    Icons Change

--- lib/destacados_widget/destacados_section.dart
+++ lib/destacados_widget/destacados_section.dart
@@ -158,6 +158,7 @@ class DestacadosSection extends StatelessWidget {
                           ),
                         ),
                       )
+
                     ],
                   );
                 },
