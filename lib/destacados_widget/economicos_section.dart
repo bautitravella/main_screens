1f5ddd98cdf4e406e319a6ef9642df1a2b490452
Bauti Pc <jbttravellita@gmail.com>
2020-03-31 19:00:19 -0300

    fix overflows

--- lib/destacados_widget/economicos_section.dart
+++ lib/destacados_widget/economicos_section.dart
@@ -310,7 +310,7 @@ class _EconmicosSectionState extends State<EconomicosSection> {
                                           0, 13, 13, 11),
                                       height: 127.0,
                                       width: SizeConfig.blockSizeHorizontal *
-                                          70,
+                                          68,
                                       decoration: BoxDecoration(
                                         /* color: Color.fromARGB(255, 241, 242, 242),*/
                                         borderRadius: BorderRadius.circular(
