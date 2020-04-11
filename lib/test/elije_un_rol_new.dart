44ab36af2e663d741ca7da9c6014df0dc0ea4adb
agustormakh <agustormakh@gmail.com>
2020-04-10 21:51:38 -0300

    elije un Rol new modificaciones para trave

--- lib/test/elije_un_rol_new.dart
+++ lib/test/elije_un_rol_new.dart
@@ -1,5 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterui/Models/Alumno.dart';
+import 'package:flutterui/Models/Padre.dart';
+import 'package:flutterui/log_in/registrar_info_usuario/subi_foto_perfil_widget.dart';
 
 final List<String> imgList = [
   "assets/images/rol-padre.png",
@@ -7,44 +10,59 @@ final List<String> imgList = [
   "assets/images/rol-universitario.png",
   ];
 
+final List<String> imgText = [
+  "Texto de la imagen 0",
+  "Texto de la imagen 1",
+  "Texto de la imagen 2",
+];
+
+final List<Widget> imgNavigatorPush = [
+  SubiFotoPerfilWidget(Padre()),
+  SubiFotoPerfilWidget(Alumno()),
+  SubiFotoPerfilWidget(Alumno()),
+];
+
 final Widget placeholder = Container(color: Colors.grey);
 
-final List child = map<Widget>(
-  imgList,
-      (index, i) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(245, 245, 245, 245),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 8.0,
-            color: Color.fromRGBO(0, 0, 0, 0.15),
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        child: Stack(children: <Widget>[
-          Image.asset(i, fit: BoxFit.cover, width: 1000.0),
-          Positioned(
-            bottom: 0.0,
-            left: 0.0,
-            right: 0.0,
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Text(
-                'No. $index image',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+
+
+final List child = map2<Widget>(imgList,imgText,imgNavigatorPush, (index, i) {
+    return GestureDetector(
+      onTap: () => print("tapped in box number $index"),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(245, 245, 245, 245),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 8.0,
+              color: Color.fromRGBO(0, 0, 0, 0.15),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          child: Stack(children: <Widget>[
+            Image.asset(i, fit: BoxFit.cover, width: 1000.0),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  imgText[index],
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
     return Container(child: Text(""));
@@ -52,10 +70,19 @@ final List child = map<Widget>(
 ).toList();
 
 
-List<T> map<T>(List list, Function handler) {
+List<T> map2<T>(List imageList,List imgText, List<Widget> imgWidget, Function handler) {
   List<T> result = [];
-  for (var i = 0; i < list.length; i++) {
-    result.add(handler(i, list[i]));
+  for (var i = 0; i < imageList.length; i++) {
+    result.add(handler(i, imageList[i]));
+  }
+
+  return result;
+}
+
+List<T> map<T>(List imageList, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < imageList.length; i++) {
+    result.add(handler(i, imageList[i]));
   }
 
   return result;
