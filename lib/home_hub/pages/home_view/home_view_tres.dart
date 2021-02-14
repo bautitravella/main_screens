57cd456b4bfae40492cdca7b59814311f908c903
agustormakh <agustormakh@gmail.com>
2021-02-14 03:47:12 -0300

    Pantalla de Home(padres fotos)

--- lib/home_hub/pages/home_view/home_view_tres.dart
+++ lib/home_hub/pages/home_view/home_view_tres.dart
@@ -2189,8 +2189,8 @@ class _HomeViewTresState extends State<HomeViewTres> {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20.0),
                 child: Image.asset(
-                  "assets/images/explora-cursos.png",
-                  fit: BoxFit.fitHeight,
+                  "assets/images/home-cursos.png",
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
@@ -2450,8 +2450,8 @@ class _HomeViewTresState extends State<HomeViewTres> {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20.0),
                 child: Image.asset(
-                  "assets/images/explora-materias.png",
-                  fit: BoxFit.fitHeight,
+                  "assets/images/home-materias.png",
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
